676f21b89eeba8b7f97905eea8960e778611b536
Berkin Ceylan <selhanberkinceylan@hotmail.com>
2019-01-22 16:41:46 +0300

    Initial Commit

---
+++ TurkishKit-UIDynamics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TurkishKit-UIDynamics
+//
+//  Created by Berkin Ceylan on 22.01.2019.
+//  Copyright © 2019 Berkin Ceylan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
